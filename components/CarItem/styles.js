86866dfcb28afd525f01d693011def71857c470e
Electro Abhinav <Abhinav>
2021-03-06 15:40:30 +0530

    Creating Basic Layout

---
+++ components/CarItem/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    carContainer: {
+        width: '100%',
+        height: '100%',
+      },
+    
+    
+      titles: {
+        marginTop: '30%',
+        width: '100%',
+        alignItems: 'center',
+      },
+    
+      title: {
+        fontSize: 40,
+        fontWeight: '700',
+      },
+    
+      subtitle: {
+        fontSize: 16,
+        color: '#5C5E62',
+      },
+    
+      image: {
+        width: '100%',
+        height: '100%',
+        resizeMode:'contain',
+        position: 'absolute'
+      }
+    
+
+});
+
+export default styles;
